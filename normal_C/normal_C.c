7c4d3e9e2c54b1316fb47ccd075ef7f47bee7629
tritdm <20520326@gm.uit.edu.vn>
2024-10-23 10:35:48 +0700

    again again

--- normal_C/normal_C.c
+++ normal_C/normal_C.c
@@ -5,5 +5,6 @@ int main()
     printf("hello Jenkins\n");
     printf("test webhook trigger\n");
     printf("test webhook trigger again\n");
+    printf("test webhook trigger again again\n");
     return 0;
 }
